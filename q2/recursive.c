da5c31c6ee016137beb0331fbda4bcdf1730a470
su2278370 <su2278370@gmail.com>
2015-10-17 23:11:13 +0800

    Recursive method for Q2
    This time,I implement this method based on the suppose of "sorted" array.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,31 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+
+char smallest_character_recursive(char str[],char c);
+
 
 int main()
 {
-    return 0;
+
+    char list[] = {'a','f','j','p','v'};
+    char input;
+    char output;
+
+    scanf(" %c",&input);
+
+    output = smallest_character_recursive(list,input);
+
+    printf("\n%c",output);
 }
+
+char smallest_character_recursive(char str[],char c)
+{
+    char first = *str;
+    if((int)*str > (int)c)
+        return *str;
+    else if (*str=='\0')
+        return first;
+    else
+        return smallest_character_recursive(str+1,c);
+}
+
