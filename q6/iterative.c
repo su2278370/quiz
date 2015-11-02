f5b9a2c3cf5390101b2cc92210b9cd05530fc3c2
su2278370 <su2278370@gmail.com>
2015-11-02 22:25:43 +0800

    Iterative method for partly reversing linked list.

--- q6/iterative.c
+++ q6/iterative.c
@@ -1,6 +1,24 @@
-/* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include "reverse_i.h"
 
-int main()
-{
-    return 0;
+int main(){
+		
+	NODE *head = NULL;
+    
+    head = insert(head,5);
+    head = insert(head,4);
+    head = insert(head,3);
+    head = insert(head,2);
+    head = insert(head,1);
+    
+    show(head);
+    printf("\n");
+    
+    head = Reverse(head,2,4);
+    
+   	show(head);
+    
+	
+	return 0;
 }
